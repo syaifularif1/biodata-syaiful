927e9b90dffac02eb513a6dd9879f7dfa01a1b46
syaifularif1 <Madridsorep179@gmail.com>
2020-09-16 21:01:14 +0700

    biodata

---
+++ pemrograman lanjut/biodata.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	 printf("<<BIODATA>>\n\n");	
+	 printf("Nama\t\t: Muhammad Syaiful Arif\n");
+	 printf("TTL\t\t: Gresik,15 Oktober 2001\n");
+	 printf("NPM \t\t: 19081010141\n");
+	 printf("Jurusan\t\t: Teknik Informatika\n");
+	 printf("Angkatan\t: 2019\n");
+	 printf("Alamat\t\t: Gresik\n");
+	 
+ return 0;
+}
